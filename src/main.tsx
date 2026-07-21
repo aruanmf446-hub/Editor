5b68f5e2a15fbe172ff0094a2a68bf8ba883f262
aruanmf446-hub <aruanmf446@gmail.com>
2026-07-20 21:43:24 -0300

    Load El Fuego editor styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { App } from './app/App';
 import './styles/global.css';
 import './styles/assets.css';
+import './styles/el-fuego.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
