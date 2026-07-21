2a12f0f06b1ad71730850aa4a734c12043572309
aruanmf446-hub <aruanmf446@gmail.com>
2026-07-21 10:59:14 -0300

    load responsive editor overrides

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from './app/App';
 import './styles/global.css';
 import './styles/assets.css';
 import './styles/el-fuego.css';
+import './styles/responsive.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
