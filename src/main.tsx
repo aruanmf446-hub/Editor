ba92530fc9194f994709730136a2e1af43ae6135
aruanmf446-hub <aruanmf446@gmail.com>
2026-07-21 15:47:11 -0300

    Carrega estilos dos painéis avançados

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from './app/App';
 import './styles/global.css';
 import './styles/assets.css';
 import './styles/el-fuego.css';
+import './styles/editor-config-panels.css';
 import './styles/runtime-enemies.css';
 import './styles/runtime-pickups.css';
 import './styles/runtime-advanced.css';
