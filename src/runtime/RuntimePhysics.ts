81412983d8ba7c8cf8da539d6672086de6790c1d
aruanmf446-hub <aruanmf446@gmail.com>
2026-07-21 09:25:54 -0300

    integrate player combat state updates

--- src/runtime/RuntimePhysics.ts
+++ src/runtime/RuntimePhysics.ts
@@ -1,15 +1,19 @@
 import type { RuntimeWorld } from './RuntimeWorld';
 import { resolvePlayerVisualState } from './RuntimePlayer';
 import { updatePlayerMovement } from './systems/PlayerMovementSystem';
+import { updatePlayerCombat } from './systems/PlayerCombatSystem';
 import { applyGravity } from './systems/GravitySystem';
 import { resolveWorldMovement } from './systems/CollisionSystem';
 import { updateCamera } from './systems/CameraSystem';
 
 export function updateRuntimeWorld(world: RuntimeWorld, delta: number) {
   if (world.paused) return;
+  updatePlayerCombat(world, delta);
   updatePlayerMovement(world, delta);
-  applyGravity(world, delta);
-  resolveWorldMovement(world, delta);
+  if (world.player.mode !== 'dead') {
+    applyGravity(world, delta);
+    resolveWorldMovement(world, delta);
+  }
   world.player.visualState = resolvePlayerVisualState(world.player);
   updateCamera(world);
 }
